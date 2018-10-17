e94a1fd9a7d05bfb2470a62dcb0ff6aff0b832a5
Luckshetty <luckshet@splm.siemens.com>
2018-10-16 23:08:08 -0400

    Add 3sum with Multiplicity

--- 62.3SumWithMultiplicity.cpp
+++ 62.3SumWithMultiplicity.cpp
@@ -33,12 +33,37 @@ Note:
 */
 
 #include <iostream>
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
 
 int threeSumMulti(vector<int>& A, int target) {
-    
+    /*
+    There are 3 possibilities
+    1. i=j=k
+    2. i==j!=k
+    3. i!=j!=k
+    */
+    unordered_map<int, long> c;
+    for(int a:A)
+        c[a]++;
+
+    long res = 0;
+
+    for(auto it:c){
+        for(auto it2:c){
+            int i=it.first, j=it2.first, k = target-i-j;
+            if(!c.count(k)) continue;
+            if(i==j && j==k)
+                res += c[i] * (c[i] - 1) * (c[i] - 2)/6;
+            else if (i==j && j!=k)
+                res += c[i] * (c[i]-1)/2 * c[k];
+            else if(i<j && j<k)
+                res += c[i] * c[j] *c[k];
+        }
+    }
+    return res % int(1e9+7);
 }
 
 int main(){
